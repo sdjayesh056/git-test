f5eadb3932272929997cecfd34648033043b7d39
jayesh <sdjayesh056@gmail.com>
2025-04-11 12:34:58 +0530

    files commit

--- my_portfolio/src/App.js
+++ my_portfolio/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Navbar from "./Components/Navbar";
+import Projects from "./Components/Projects";
+import Skills from "./Components/Skills";
+import Testimonials from "./Components/Testimonials";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <About />
+      <Contact />
+      <Navbar />
+      <Projects />
+      <Skills />
+      <Testimonials />
     </div>
   );
 }
